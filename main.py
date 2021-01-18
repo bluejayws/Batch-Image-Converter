138b3be3c7760a993a8e342d1435a2c7b326f516
evgnngve-ii <66582478+evgnngve-ii@users.noreply.github.com>
2021-01-18 00:05:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 import urllib.request
 import PySimpleGUI as sg
 import sys
+from urllib.error import HTTPError
 
 #Reads a text file with links in them, one link per line.
 #Returns a list with these links
@@ -24,13 +25,13 @@ def concatenate_link(link):
 
 
 if __name__ == '__main__':
-    sg.theme('LightTeal')
+    sg.theme('DarkPurple3')
     #Get parameters
     if len(sys.argv) == 1:
         event, values = sg.Window('Photo Batch Downloader v1 :)',
                                   [[sg.Text('Source file: ')],
                                    [sg.In(), sg.FileBrowse()],
-                                   [sg.Text('Output folder')],
+                                   [sg.Text('Output folder: ')],
                                     [sg.In(), sg.FolderBrowse()],
                                    [sg.Open(), sg.Cancel()]]).read(close=True)
         fname = values[0]
@@ -40,14 +41,21 @@ if __name__ == '__main__':
     textFileName = fname
 
     saveFolder = values[1]
-    
+
     #Get links as a list
     list_of_links = parse_links_text(textFileName)
 
     #Find and downloader image links
     image_num = 1;
     for link in list_of_links:
-        urllib.request.urlretrieve(concatenate_link(link), concatenateFileName(image_num,saveFolder))
-        image_num += 1
+        try:
+            urllib.request.urlretrieve(concatenate_link(link), concatenateFileName(image_num, saveFolder))
+        except HTTPError as httperr:
+            if httperr.code == 404:
+                print(concatenate_link(link) + " not downloaded! 404 Error for the link")
+                image_num += 1
+
+        #urllib.request.urlretrieve(concatenate_link(link), concatenateFileName(image_num,saveFolder))
+        #image_num += 1
 
 
