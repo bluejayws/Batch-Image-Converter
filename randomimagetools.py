5c97e2da5493c2aacbe838aec7216fa982a30c5d
bluejayws <66582478+bluejayws@users.noreply.github.com>
2021-11-13 01:13:28 -0600

    cleaned up code

--- randomimagetools.py
+++ randomimagetools.py
@@ -48,8 +48,6 @@ class MainWindow(QMainWindow):
         self.convert_to_png_button.clicked.connect(self.convert_folder_to_png)
         self.convert_to_png_button.show()
 
-        # ToDo: Open folder in finder option
-
         # Put the find folder button and folder selected button together
         layout_button_and_directory_name.addWidget(self.select_a_folder_button)
         layout_button_and_directory_name.addWidget(self.directory_label)
@@ -59,49 +57,42 @@ class MainWindow(QMainWindow):
         layout_of_vertical_actions.addWidget(self.convert_to_png_button)
 
         widget.setLayout(layout_of_vertical_actions)
-
         self.setCentralWidget(widget)
 
     # Prompts user to select a folder, stores the given folder path and displays chosen path to user
     def select_folder_prompt(self):
-        # print("select a folder prompt button has been clicked")
         # Append a "/" otherwise it will mix the folder name and containing image file together
         directory = str(QFileDialog.getExistingDirectory(self, "Select Directory")) + "/"
-        #print("bug here maybe: %s" % directory)
         # You already passed the object. Just store the string into the object and update it there
         self.directory_label.setText(directory)
         self.directory_path_name = directory
-        # print(directory)
 
     # Given a non-empty folder path, converts all jpg images in it to png.
     def convert_folder_to_png(self):
-        # Get list of image files in the given folder
-        # Todo: Add conversion to PNG of other file types, f.ex gif, webmp, etc
-        # print("converting %s to png..." % self.directory_path_name)
-        image_list = []
         # ToDo: Remove duplicate adding of image paths to image_list
+        # Todo: Add conversion to PNG of other file types, f.ex gif, webmp, etc
+
+        # Get list of image files in the given folder
+        image_list = []
         for root, dirs, files in os.walk(self.directory_path_name, topdown=True):
             for filename in files:
                 if '.jpg' in filename:
                     absolute_path = self.directory_path_name + filename
-                    # print(absolutePath)
+                    # Avoid adding duplicates
                     if absolute_path not in image_list:
-                        # print("appending %s to imageList " % absolutePath)
                         image_list.append(absolute_path)
 
         for jpg_image_path in image_list:
             # Get absolute path
             abs_path = os.path.abspath(jpg_image_path)
 
-            # save as png
+            # Save as png
             jpg_image_to_png = Image.open(abs_path)
             jpg_image_to_png.save(rename_to_png(abs_path))
 
             # Todo: Store png images in a new folder?
             # Todo: Add option to delete jpg images
             # Todo: Add a popup or some sort of progress mechaniism to delete photos
-
-
 
 
 app = QApplication(sys.argv)
