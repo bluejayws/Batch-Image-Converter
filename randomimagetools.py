50f06b061992f3a9df676181771a5eab067d2433
bluejayws <66582478+bluejayws@users.noreply.github.com>
2021-11-13 01:40:25 -0600

    Added a "Show in File Browser" option
    just tested on mac

--- randomimagetools.py
+++ randomimagetools.py
@@ -1,6 +1,7 @@
 # coded by: @bluejayws (Antonio G-B), internet, stackoverflow, etc.
 import os
 import sys
+import subprocess
 
 from PIL import Image
 from PyQt6.QtWidgets import (
@@ -9,26 +10,28 @@ from PyQt6.QtWidgets import (
 )
 
 
+# Takes a path name to an image file and returns a new path name with the extension changed to .png
+# ToDo: Add support for more than png (?)
 def rename_to_png(path):
-    # print("Path before rename: %s" % path)
     split_file_at_dot = path.split(".")
     new_path = split_file_at_dot[0] + "_PNG.png"
-    # print("Path after rename: %s" % new_path)
     return new_path
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("📸 Random Image Tools V1 🔨")
+        self.setWindowTitle("📸 Random Image Tools V1.1 🔨")
 
         # to store the directory path for later use
         self.directory_path_name = ""
 
-        # load a layout
-        layout_button_and_directory_name = QHBoxLayout()
-        layout_of_vertical_actions = QVBoxLayout()
-        # widget
+        # Set up the layouts
+        layer_one = QHBoxLayout()  # First line of buttons
+        layer_two = QHBoxLayout()  # Second line of buttons
+        vertical_layout_parent = QVBoxLayout()
+
+        # Main widget
         widget = QWidget()
 
         # Create a label to show directory name
@@ -48,24 +51,42 @@ class MainWindow(QMainWindow):
         self.convert_to_png_button.clicked.connect(self.convert_folder_to_png)
         self.convert_to_png_button.show()
 
+        # Show folder after conversion
+        self.show_folder_button = QPushButton()
+        self.show_folder_button.setText("Open chosen directory in Finder")
+        self.show_folder_button.clicked.connect(self.open_folder)
+        self.show_folder_button.show()
+
         # Put the find folder button and folder selected button together
-        layout_button_and_directory_name.addWidget(self.select_a_folder_button)
-        layout_button_and_directory_name.addWidget(self.directory_label)
+        layer_one.addWidget(self.select_a_folder_button)
+        layer_one.addWidget(self.directory_label)
+
+        # Put the convert button and open-in-finder button together
+        layer_two.addWidget(self.convert_to_png_button)
+        layer_two.addWidget(self.show_folder_button)
 
         # Put the "convert to png" button beneath
-        layout_of_vertical_actions.addLayout(layout_button_and_directory_name)
-        layout_of_vertical_actions.addWidget(self.convert_to_png_button)
+        vertical_layout_parent.addLayout(layer_one)
+        vertical_layout_parent.addLayout(layer_two)
 
-        widget.setLayout(layout_of_vertical_actions)
+        widget.setLayout(vertical_layout_parent)
         self.setCentralWidget(widget)
 
     # Prompts user to select a folder, stores the given folder path and displays chosen path to user
     def select_folder_prompt(self):
         # Append a "/" otherwise it will mix the folder name and containing image file together
         directory = str(QFileDialog.getExistingDirectory(self, "Select Directory")) + "/"
-        # You already passed the object. Just store the string into the object and update it there
+        # Note to self: You already passed the object. Just store the string into the object and update it there
         self.directory_label.setText(directory)
         self.directory_path_name = directory
+
+    # Given a valid path name, will open it in the Folder browser app ( Only tested on MacOS so far)
+    def open_folder(self):
+        if self.directory_path_name != "":
+            subprocess.call(["open", "-R", self.directory_path_name])
+        else:
+            # ToDo: Turn this into a error display that the user can see
+            print("Not a valid directory :,(")
 
     # Given a non-empty folder path, converts all jpg images in it to png.
     def convert_folder_to_png(self):
@@ -82,17 +103,21 @@ class MainWindow(QMainWindow):
                     if absolute_path not in image_list:
                         image_list.append(absolute_path)
 
-        for jpg_image_path in image_list:
-            # Get absolute path
-            abs_path = os.path.abspath(jpg_image_path)
+        if len(image_list) > 0:  # There's at least on
+            for jpg_image_path in image_list:
+                # Get absolute path
+                abs_path = os.path.abspath(jpg_image_path)
 
-            # Save as png
-            jpg_image_to_png = Image.open(abs_path)
-            jpg_image_to_png.save(rename_to_png(abs_path))
+                # Save as png
+                jpg_image_to_png = Image.open(abs_path)
+                jpg_image_to_png.save(rename_to_png(abs_path))
 
-            # Todo: Store png images in a new folder?
-            # Todo: Add option to delete jpg images
-            # Todo: Add a popup or some sort of progress mechaniism to delete photos
+                # Todo: Store png images in a new folder?
+                # Todo: Add option to delete jpg images
+                # Todo: Add a popup or some sort of progress mechaniism to delete photos
+        else:
+            # ToDo: Let user know that folder had no jpeg files in it, or any other type.
+            print("There was no .jpeg, or image files that aren't in PNG format")
 
 
 app = QApplication(sys.argv)
